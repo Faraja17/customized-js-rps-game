c919f8edf3f86f7c6cc8bbfa9d176203d7fce725
Faraja17 <farajakthompson@gmail.com>
2022-07-16 16:40:53 -0400

    Customized for my little cousins <3
    Changed the color scheme to the requested pink and purple. Added surprise fruits to the move icons

---
+++ scripts/index.js
@@ -0,0 +1,187 @@
+// variables to keep track of moves and the number of wins, losses, ties
+let wins = 0;
+let losses = 0;
+let ties = 0;
+let key = "";
+let computerMove = "";
+
+// game intro detecting button clicks
+
+for (let i = 0; i <document.querySelectorAll(".move").length; i++) {
+        
+    document.querySelectorAll(".move")[i].addEventListener('click', function() {
+
+        let buttonInnerHTML = this.innerHTML;
+
+        shake();
+        setTimeout(function() {
+            playerChoice(buttonInnerHTML);
+            computerChoice();
+            gamePlay(buttonInnerHTML, computerMove);
+        }, 1600); /* milliseconds, so .16 seconds*/
+    });
+}      
+
+// detecting keyboard presses
+
+document.addEventListener("keydown", function(event) {
+
+    shake();
+    setTimeout(function() {
+      playerChoice(event.key);
+      computerChoice();
+      gamePlay(event.key, computerMove);
+    }, 1600); /* milliseconds, so .16 seconds*/
+});
+
+
+const playerChoice = (key) => {
+// display what the player chose
+   switch (key) {
+    case "r":
+        document.querySelector(".playerMove").setAttribute("src", "images/playerRock.png");
+        break;
+
+    case "p":
+        document.querySelector(".playerMove").setAttribute("src", "images/playerPaper.png");
+        break;
+
+    case "s":
+        document.querySelector(".playerMove").setAttribute("src", "images/playerScissors.png");
+        break;
+    }
+}
+
+const computerChoice = () => {
+// display what the computer chose
+    let randomNumber = Math.floor(Math.random() * 3);
+    if(randomNumber === 0) {
+        computerMove = "r";
+        document.querySelector(".compMove").setAttribute("src", "images/computerRock.png");
+    } 
+    else if(randomNumber === 1) {
+        computerMove = "p";
+        document.querySelector(".compMove").setAttribute("src", "images/computerPaper.png");
+    } 
+    else if(randomNumber === 2) {
+        computerMove = "s";
+        document.querySelector(".compMove").setAttribute("src", "images/computerScissors.png");
+    }
+}
+
+const gamePlay = (key, computerMove) => {
+// compare player vs. computer, then display and record the total wins/losses/ties
+
+// tie
+    if(key == computerMove) {
+        ties++;
+        document.querySelector(".report").innerHTML = `Losses: ${losses}, 🚩Ties: ${ties}🚩, Wins: ${wins}`;  
+    }
+
+// win
+ 
+    else if(key === "r" && computerMove === "s") {
+        wins++;
+        document.querySelector(".report").innerHTML = `Losses: ${losses}, Ties: ${ties}, 🚩Wins: ${wins}🚩`;   
+    } 
+    else if(key === "p" && computerMove === "r") {
+        wins++;
+        document.querySelector(".report").innerHTML = `Losses: ${losses}, Ties: ${ties}, 🚩Wins: ${wins}🚩`;
+    } 
+    else if(key === "s" && computerMove === "p") {
+        wins++;
+        document.querySelector(".report").innerHTML = `Losses: ${losses}, Ties: ${ties}, 🚩Wins: ${wins}🚩`;
+    }
+
+// loss
+    else if(key === "r" && computerMove === "p") {
+        losses++;
+        document.querySelector(".report").innerHTML = `🚩Losses: ${losses}🚩, Ties: ${ties}, Wins: ${wins}`;
+    } 
+    else if(key === "p" && computerMove === "s") {
+        losses++;
+        document.querySelector(".report").innerHTML = `🚩Losses: ${losses}🚩, Ties: ${ties}, Wins: ${wins}`;
+    } 
+    else if(key === "s" && computerMove === "r") {
+        losses++;
+        document.querySelector(".report").innerHTML = `🚩Losses: ${losses}🚩, Ties: ${ties}, Wins: ${wins}`;
+    }
+}
+
+const shake = () => {
+    //change back to rock
+        document.querySelector(".compMove").setAttribute("src", "images/computerFist.png");
+        document.querySelector(".playerMove").setAttribute("src", "images/playerFist.png");
+    
+    //move up
+    let compFist = document.querySelector(".compMove");
+    let container = document.querySelector(".animation");
+
+    compFist.classList.add("up");
+    container.classList.add("containerUp");
+    
+    //move down
+    setTimeout(function() {
+        compFist.classList.remove("up");
+        container.classList.remove("containerUp");
+    }, 250); /* milliseconds, so .025 seconds*/
+
+    //move down
+    setTimeout(function() {
+        compFist.classList.add("up");
+        container.classList.add("containerUp");
+    }, 500); /* milliseconds, so .05 seconds*/
+
+    //move down
+    setTimeout(function() {
+        compFist.classList.remove("up");
+        container.classList.remove("containerUp");
+    }, 750); /* milliseconds, so .075 seconds*/
+
+    //move down
+    setTimeout(function() {
+        compFist.classList.add("up");
+        container.classList.add("containerUp");
+    }, 1000); /* milliseconds, so .1 seconds*/
+
+    //move down
+    setTimeout(function() {
+        compFist.classList.remove("up");
+        container.classList.remove("containerUp");
+    }, 1250); /* milliseconds, so .125 seconds*/
+
+    let playerFist = document.querySelector(".playerMove");
+
+    playerFist.classList.add("up");
+    container.classList.add("containerUp");
+    
+    //move down
+    setTimeout(function() {
+        playerFist.classList.remove("up");
+        container.classList.remove("containerUp");        
+    }, 250); /* milliseconds, so .025 seconds*/
+
+    //move down
+    setTimeout(function() {
+        playerFist.classList.add("up");
+        container.classList.add("containerUp");
+    }, 500); /* milliseconds, so .05 seconds*/
+
+    //move down
+    setTimeout(function() {
+        playerFist.classList.remove("up");
+        container.classList.remove("containerUp");   
+    }, 750); /* milliseconds, so .075 seconds*/
+
+    //move down
+    setTimeout(function() {
+        playerFist.classList.add("up");
+        container.classList.add("containerUp");
+    }, 1000); /* milliseconds, so .1 seconds*/
+
+    //move down
+    setTimeout(function() {
+        playerFist.classList.remove("up");
+        container.classList.remove("containerUp");   
+    }, 1250); /* milliseconds, so .125 seconds*/
+}
